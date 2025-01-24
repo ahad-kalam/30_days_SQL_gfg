d08c7dfc60951c27128aca4fe5b979a1af7f59cb
ahad-kalam <abdulahadkalam37@gmail.com>
2025-01-24 19:25:36 +0530

    Add files via upload

---
+++ day1.sql
@@ -0,0 +1,17 @@
+-- Creating Database
+
+/* The CREATE DATABASE statement is a foundational SQL command used to create new databases in SQL-based Database Management Systems (DBMS),
+ including MySQL, PostgreSQL, SQL Server, and others. Understanding how to use this command effectively is crucial for developers, database
+ administrators, and anyone working with relational databases.*/
+
+create database day1;
+drop database day1;
+show databases;
+use day1;
+
+/*
+Important Points About SQL CREATE DATABASE
+Database Naming Conventions:Avoid using spaces; instead, opt for underscores (_). Example: my_database is preferable to my database
+Maximum Length of Database Names:a database name in SQL is 128 characters. Ensure that your database name is both clear and concise
+
+*/
